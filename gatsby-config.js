687e517da8bf3e2bf121a98dd9bcdb607fd55833
Bryan Ruano <bruanosa@gmail.com>
2021-03-11 13:49:05 -0500

    added google fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,21 @@
 module.exports = {
-   plugins: 
-      ['gatsby-plugin-sass'],
+   plugins: [
+      'gatsby-plugin-sass',
+      {
+         resolve: `gatsby-plugin-prefetch-google-fonts`,
+         options: {
+           fonts: [
+             {
+               family: `Roboto`,
+               variants: [`300` , `400`, `700`, `900`]
+             },
+             {
+                family: `Lobster`,
+                variants: [`400`, `700`]
+             }
+            ],
+         }
+      }
+      //insert more plugins here
+      ]
 }
