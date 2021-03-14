63829aff1de2e214a1329c11b3b01ee79f4c1197
Bryan Ruano <bruanosa@gmail.com>
2021-03-14 17:36:12 -0400

    optimized images, and added swipe gestures

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -1,16 +1,84 @@
 import  React, {useState} from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMoon} from '@fortawesome/free-solid-svg-icons';
-import Mockup from "../images/GenesisMockUp.png";
+import {StaticImage} from 'gatsby-plugin-image';
 
+//components imports
 import Navbar from '../Components/Navbar';
 import Button from '../Components/Button';
 import Menu from '../Components/Menu';
 import Stars from '../Components/Stars';
 
+//function imports
+import {useEventListener} from '../Functions/eventHook.js';
+
 export default function Landing(props) {
 
+    //state variables and functions
     const [isMenuOpen, setMenu] = useState(false);
+    const [touchStart, setTouchStart] = useState(null);
+    const [touchEnd, setTouchEnd] = useState(null);
+    const [sliderIsRight, setSlider] = useState(true);
+
+    //lets us know if client's device is a touch screen
+    const isTouchDevice = "ontouchstart" in document.documentElement;
+   
+    //setting up our on touch start event listener
+    useEventListener("slider-touch-area", 'touchstart', (e) => {
+        e.preventDefault();
+        setTouchStart(e.touches[0].clientX);
+    });
+
+    //setting up our on touch end event listener
+    useEventListener("slider-touch-area",'touchend', (e) => {
+        e.preventDefault();
+        setTouchEnd(e.changedTouches[0].pageX);
+       
+        //case when swiped right and slider is currently on the left side
+        if(touchEnd - touchStart > 0 && !sliderIsRight){
+            setSlider(!sliderIsRight);
+            props.switchTheme();
+            console.log("swiped right");
+        }
+        //case when swiped right and slider is currently on the right side
+        else if(touchEnd - touchStart < 0 && sliderIsRight){
+            setSlider(!sliderIsRight);
+            props.switchTheme();
+            console.log("swiped left");
+        }
+        else{
+            //case when the user taps roughly in the same area give or take 5 pixels
+            if(Math.abs(touchEnd - touchStart) > 0 && Math.abs(touchEnd - touchStart) < 5){
+                setSlider(!sliderIsRight);
+                props.switchTheme();
+            }
+        }
+    });
+
+    //conditionally renders the appropriate slider component
+    function renderSlider(){
+        //in the case when it's a touch device we don't want to have an onClick event handler
+        if(isTouchDevice){
+            return(
+                <div class={`slider ${props.getTheme()}`}>
+                    <div class="slider-ball"> 
+                        <FontAwesomeIcon icon={faMoon} className="moon-icon"/>
+                    </div>
+                </div>
+            );
+  
+        }
+        //in the case when its not a touch device we want to have an onClick event handler
+        else{
+            return(
+                <div onClick={() => props.switchTheme()} class={`slider ${props.getTheme()}`}>
+                    <div class="slider-ball"> 
+                        <FontAwesomeIcon icon={faMoon} className="moon-icon"/>
+                    </div>
+                </div>
+            );
+        }
+    }
 
     return (
         <div class={`landing-container ${props.getTheme()}`}>
@@ -20,11 +88,8 @@ export default function Landing(props) {
             </div>
            
             <div class="grid-item-slider">
-                <div onClick={() => props.switchTheme()} class={`slider ${props.getTheme()}`}>
-                    <div class="slider-ball"> 
-                        <FontAwesomeIcon icon={faMoon} className="moon-icon"/>
-                    </div>
-                </div>
+                <div id="slider-touch-area" className={(isTouchDevice) ? "visible" : "hidden"}/>
+                {renderSlider()}
             </div>
             
             <div class="grid-item-landing-text">
@@ -38,7 +103,15 @@ export default function Landing(props) {
             </div>
 
             <div class="grid-item-landing-img"> 
-                <img class="mockup-img" src={Mockup} alt="responsive website showcase"/>
+                <StaticImage
+                    src="../images/GenesisMockup.png"
+                    alt="responsive website design showcase"
+                    loading="eager"
+                    layout="constrained"
+                    placeholder="blurred"
+                    className="mockup-img"
+                    objectFit="contain"
+                />
             </div>
 
 
