8a749c746ec9eacebbc3fe2a98aa47951fb46a16
Bryan Ruano <bruanosa@gmail.com>
2021-03-24 12:24:54 -0400

    added active navlink within same page

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -9,11 +9,11 @@ export default function Menu(props) {
         <div class={`drop-down-menu ${props.isMenuOpen ? "open" : "closed"}`}>
             <nav>
                 <ul>
-                    <li onClick={handleClick}><Link class="nav-link" activeClassName="active-link" to="/">Home</Link></li>
-                    <li onClick={handleClick}><Link class="nav-link" activeClassName="active-link" to="/#about">About</Link></li>
-                    <li onClick={handleClick}><Link class="nav-link" activeClassName="active-link" to="/#skills">Skills</Link></li>
-                    <li onClick={handleClick}><Link class="nav-link" activeClassName="active-link" to="/#work">Work</Link></li>
-                    <li onClick={handleClick}><Link class="nav-link" activeClassName="active-link" to="/#contact">Contact</Link></li>
+                    <li><Link className={`nav-link ${props.landingActive ? 'active' : ''}`} activeClassName="active-link" to="/">Home</Link></li>
+                    <li><Link className={`nav-link ${props.aboutActive ? 'active' : ''}`} activeClassName="active-link" to="/#about">About</Link></li>
+                    <li><Link className={`nav-link ${props.skillsActive ? 'active' : ''}`} activeClassName="active-link" to="/#skills">Skills</Link></li>
+                    <li><Link className={`nav-link ${props.workActive ? 'active' : ''}`} activeClassName="active-link" to="/#work">Work</Link></li>
+                    <li><Link class={`nav-link ${props.contactActive ? 'active' : ''}`} activeClassName="active-link" to="#contact">Contact</Link></li>
                 </ul>
             </nav>
         </div>
